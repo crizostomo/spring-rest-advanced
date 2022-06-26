12d0f3addb43894d8c28d3727e8a8cf3003d79a9
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-25 21:27:33 -0300

    2.15 Optional dependency with Autowired = False

--- src/main/java/com/developer/beverageapi/notification/EmailNotificator.java
+++ src/main/java/com/developer/beverageapi/notification/EmailNotificator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
 public class EmailNotificator implements Notificator {
 
     private boolean upperCase;
--- src/main/java/com/developer/beverageapi/service/ClientActivationService.java
+++ src/main/java/com/developer/beverageapi/service/ClientActivationService.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class ClientActivationService {
 
-    @Autowired
+    @Autowired(required = false)
     private Notificator notificator;
 
     public ClientActivationService(Notificator notificator) {
         this.notificator = notificator;
     }
 
-    public void active(Client client){
+    public void active(Client client) {
         client.isActive();
 
-        notificator.notifyEmail(client, "Your record is active");
+        if (notificator != null) {
+
+            notificator.notifyEmail(client, "Your record is active");
+        } else {
+            System.out.println("There is no notificator, but client has been activated");
+        }
     }
 }
