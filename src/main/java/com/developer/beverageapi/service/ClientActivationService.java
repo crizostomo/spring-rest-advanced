0c13421bef082c9cdf31d85516d61c087539d5ab
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-28 08:20:04 -0300

    2.23 - Working on some configs for application.yml

--- src/main/java/com/developer/beverageapi/service/ClientActivationService.java
+++ src/main/java/com/developer/beverageapi/service/ClientActivationService.java
@@ -1,9 +1,6 @@
 package com.developer.beverageapi.service;
 
 import com.developer.beverageapi.model.Client;
-import com.developer.beverageapi.notification.Notificator;
-import com.developer.beverageapi.notification.NotifierType;
-import com.developer.beverageapi.notification.UrgencyLevel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
