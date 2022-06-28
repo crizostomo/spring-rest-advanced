d3135f12d720943e423c5334a12e26becc093ef5
crizostomo <diogo_crizostomo@hotmail.com>
2022-06-27 22:56:56 -0300

    2.21 - Creating callback methods in beans

--- src/main/java/com/developer/beverageapi/service/ClientActivationService.java
+++ src/main/java/com/developer/beverageapi/service/ClientActivationService.java
@@ -8,12 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class ClientActivationService {
 
     @NotifierType(UrgencyLevel.URGENT)
     @Autowired
     private Notificator notificator;
+
+//    @PostConstruct
+    public void init(){
+        System.out.println("INIT");
+    }
+
+//    @PreDestroy
+    public void destroy(){
+        System.out.println("DESTROY");
+    }
 
     public void active(Client client) {
         client.isActive();
