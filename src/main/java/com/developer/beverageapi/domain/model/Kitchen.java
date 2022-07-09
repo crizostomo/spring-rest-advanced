cdbc2baf3e7990f76622397fd17ec5f0ea0c7f1f
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-09 18:58:54 -0300

    Mapping the entity id to autoincrement

--- src/main/java/com/developer/beverageapi/domain/model/Kitchen.java
+++ src/main/java/com/developer/beverageapi/domain/model/Kitchen.java
@@ -1,9 +1,6 @@
 package com.developer.beverageapi.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +8,7 @@ import java.util.Objects;
 public class Kitchen {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column (name = "name", length = 30) //Optional if you want to use the same name
--- src/main/java/com/developer/beverageapi/domain/model/Restaurant.java
+++ src/main/java/com/developer/beverageapi/domain/model/Restaurant.java
@@ -1,9 +1,6 @@
 package com.developer.beverageapi.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -12,6 +9,7 @@ import java.util.Objects;
 public class Restaurant {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column (name = "name") //Optional if you want to use the same name
