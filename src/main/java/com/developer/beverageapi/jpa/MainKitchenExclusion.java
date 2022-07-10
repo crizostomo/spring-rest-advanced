8d5fdffd29eba882e7846e09f970ba00ef082248
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-10 15:27:52 -0300

    3.11 | Updating an object in the DB

---
+++ src/main/java/com/developer/beverageapi/jpa/MainKitchenExclusion.java
@@ -0,0 +1,26 @@
+package com.developer.beverageapi.jpa;
+
+import com.developer.beverageapi.BeverageApiApplication;
+import com.developer.beverageapi.domain.model.Kitchen;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class MainKitchenAlteration {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(BeverageApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        KitchenRecord kitchenRecord = applicationContext.getBean(KitchenRecord.class);
+
+        Kitchen kitchen1 = new Kitchen();
+        kitchen1.setId(1L);
+        kitchen1.setName("Brazilian");
+
+        kitchen1 = kitchenRecord.add(kitchen1);
+
+        System.out.printf("%d - %s\n", kitchen1.getId(), kitchen1.getName());
+    }
+}
