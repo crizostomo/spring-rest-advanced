394f39da93505990002b669e3137d23b2a4fda18
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-16 19:08:36 -0300

    4.13 | Implementing content registration to return JSON or XML

--- src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
+++ src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
@@ -3,6 +3,7 @@ package com.developer.beverageapi.controller;
 import com.developer.beverageapi.domain.model.Kitchen;
 import com.developer.beverageapi.domain.repository.RepositoryKitchen;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ControllerKitchen {
 
     @Autowired
     private RepositoryKitchen repositoryKitchen;
 
+//    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @GetMapping
     public List<Kitchen> list(){
         return repositoryKitchen.listAll();
