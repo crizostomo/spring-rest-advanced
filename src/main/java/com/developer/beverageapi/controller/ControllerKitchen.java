e23af64e13cd4f9fe010d890d09f9141eef7af30
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-18 07:54:29 -0300

    4.20 | Manipulating the HTTP response with ResponseEntity

--- src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
+++ src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
@@ -4,8 +4,10 @@ import com.developer.beverageapi.domain.model.Kitchen;
 import com.developer.beverageapi.domain.model.XMLWrapperKitchens;
 import com.developer.beverageapi.domain.repository.RepositoryKitchen;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,10 +30,19 @@ public class ControllerKitchen {
         return new XMLWrapperKitchens(repositoryKitchen.listAll());
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
+//    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{kitchenId}")
-    public Kitchen search(@PathVariable Long kitchenId){
-        return repositoryKitchen.searchById(kitchenId);
-    }
+    public ResponseEntity<Kitchen> search(@PathVariable Long kitchenId){
+        Kitchen kitchen = repositoryKitchen.searchById(kitchenId);
 
+//        return ResponseEntity.status(HttpStatus.OK).body(kitchen);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/kitchens");
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(headers)
+                .build();
+    }
 }
