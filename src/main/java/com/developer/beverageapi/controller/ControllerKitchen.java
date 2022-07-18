3e56250c209a2db9d34ff7b5f0531fde378f0edf
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-18 07:42:30 -0300

    4.19 | Using the Annotation ResponseStatus

--- src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
+++ src/main/java/com/developer/beverageapi/controller/ControllerKitchen.java
@@ -4,11 +4,9 @@ import com.developer.beverageapi.domain.model.Kitchen;
 import com.developer.beverageapi.domain.model.XMLWrapperKitchens;
 import com.developer.beverageapi.domain.repository.RepositoryKitchen;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +28,7 @@ public class ControllerKitchen {
         return new XMLWrapperKitchens(repositoryKitchen.listAll());
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{kitchenId}")
     public Kitchen search(@PathVariable Long kitchenId){
         return repositoryKitchen.searchById(kitchenId);
