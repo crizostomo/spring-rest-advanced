3c7391cb63eca783fa008902a4eea914e88744ae
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-18 07:13:44 -0300

    4.15 | Using the annotations JsonRootName, JsonProperty and JsonIgnore

--- src/main/java/com/developer/beverageapi/domain/model/Kitchen.java
+++ src/main/java/com/developer/beverageapi/domain/model/Kitchen.java
@@ -1,10 +1,14 @@
 package com.developer.beverageapi.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+//@JsonRootName("Kitchen")  The representation in the API | Postman
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -16,6 +20,8 @@ public class Kitchen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @JsonIgnore - It ignores this property
+    @JsonProperty("name") //The representation in the API | Postman
     @Column (name = "name", length = 30, nullable = false) //Optional if you want to use the same name
     private String name;
 
