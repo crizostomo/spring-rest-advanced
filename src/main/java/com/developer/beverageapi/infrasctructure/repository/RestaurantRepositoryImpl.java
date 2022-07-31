dd764ac00a9d0732847123ea9d8f314a2296f04d
crizostomo <diogo_crizostomo@hotmail.com>
2022-07-31 20:53:55 -0300

    5.12 | Implementing a dynamic consult with JPQL

--- src/main/java/com/developer/beverageapi/infrasctructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/developer/beverageapi/infrasctructure/repository/RestaurantRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.developer.beverageapi.infrasctructure.repository;
 import com.developer.beverageapi.domain.model.Restaurant;
 import com.developer.beverageapi.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -18,13 +21,31 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     @Override
     public List<Restaurant> find(String name, BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
 
-        var jpql = "from Restaurant where name like :name " +
-                "and delivery between :initialFee and :finalFee";
+        var jpql = new StringBuilder();
+        jpql.append("from Restaurant where 0 = 0 ");
 
-        return manager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%" + name + "%")
-                .setParameter("initialFee", initialDeliveryFee)
-                .setParameter("finalFee", finalDeliveryFee)
-                .getResultList();
+        var parameters = new HashMap<String, Object>();
+
+        if (StringUtils.hasLength(name)) {
+            jpql.append("and name like :name ");
+            parameters.put("name", "%" + name + "%");
+        }
+
+        if (initialDeliveryFee != null) {
+            jpql.append("and delivery >= :initialFee ");
+            parameters.put("initialFee", initialDeliveryFee);
+        }
+
+        if (finalDeliveryFee != null) {
+            jpql.append("and delivery <= :finalFee ");
+            parameters.put("finalFee", finalDeliveryFee);
+        }
+
+        TypedQuery<Restaurant> query = manager.createQuery(jpql.toString(), Restaurant.class);
+
+        parameters.forEach((key, value) -> query.setParameter(key, value));
+
+        return query.getResultList();
+
     }
 }
