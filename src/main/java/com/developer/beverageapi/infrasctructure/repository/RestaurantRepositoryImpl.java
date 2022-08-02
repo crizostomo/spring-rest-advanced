1eff75233a472822428646f6fb0dc94c5fb2f312
crizostomo <diogo_crizostomo@hotmail.com>
2022-08-02 08:06:51 -0300

    5.14 | Adding restrictions in the where clause with CRITERIA API

--- src/main/java/com/developer/beverageapi/infrasctructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/developer/beverageapi/infrasctructure/repository/RestaurantRepositoryImpl.java
@@ -3,15 +3,15 @@ package com.developer.beverageapi.infrasctructure.repository;
 import com.developer.beverageapi.domain.model.Restaurant;
 import com.developer.beverageapi.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -24,9 +24,19 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     public List<Restaurant> find(String name, BigDecimal initialDeliveryFee, BigDecimal finalDeliveryFee) {
 
         CriteriaBuilder builder = manager.getCriteriaBuilder();
-        
+
         CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
-        criteria.from(Restaurant.class); // from Restaurant
+        Root<Restaurant> root = criteria.from(Restaurant.class);// from Restaurant
+
+        Predicate predicateName = builder.like(root.get("name"), "%" + name + "%");
+
+        Predicate initialFeePredicate = builder
+                .greaterThanOrEqualTo(root.get("delivery"), initialDeliveryFee);
+
+        Predicate finalFeePredicate = builder
+                .lessThanOrEqualTo(root.get("delivery"), finalDeliveryFee);
+
+        criteria.where(predicateName, initialFeePredicate, finalFeePredicate);
 
         TypedQuery<Restaurant> query = manager.createQuery(criteria);
         return query.getResultList();
