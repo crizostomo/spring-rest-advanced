d5a8d87b9e1dbbedb4f1de87cb215f1de9f6e0c0
crizostomo <diogo_crizostomo@hotmail.com>
2022-08-21 09:39:36 -0300

    7.9 | Creating the migration based on the DDL generated by schema generation - Tables Created

--- src/main/resources/db/migration/V004__create_several_tables.sql
+++ src/main/resources/db/migration/V004__create_several_tables.sql
@@ -10,3 +10,71 @@ create table `group` (
 
 	primary key (id)
 ) engine=InnoDB default charset=utf8;
+
+create table group_permission (
+	group_id bigint not null,
+	permission_id bigint not null,
+
+	primary key (group_id, permission_id)
+) engine=InnoDB default charset=utf8;
+
+create table permission (
+	id bigint not null auto_increment,
+	description varchar(60) not null,
+	name varchar(100) not null,
+
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+create table product (
+	id bigint not null auto_increment,
+	restaurant_id bigint not null,
+	name varchar(80) not null,
+	description text not null,
+	price decimal(10,2) not null,
+	active tinyint(1) not null,
+
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+create table restaurant (
+	id bigint not null auto_increment,
+	kitchen_id bigint not null,
+	name varchar(80) not null,
+	delivery decimal(10,2) not null,
+	updated_date datetime not null,
+	record_date datetime not null,
+
+	address_city_id bigint,
+	address_cep varchar(9),
+	address_street varchar(100),
+	address_number varchar(20),
+	address_complement varchar(60),
+	address_neighborhood varchar(60),
+
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+create table restaurant_payment (
+	restaurant_id bigint not null,
+	payment_id bigint not null,
+
+	primary key (restaurant_id, payment_id)
+) engine=InnoDB default charset=utf8;
+
+create table `user` (
+	id bigint not null auto_increment,
+	name varchar(80) not null,
+	email varchar(255) not null,
+	password varchar(255) not null,
+	record_date datetime not null,
+
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+create table user_group (
+	user_id bigint not null,
+	group_id bigint not null,
+
+	primary key (user_id, group_id)
+) engine=InnoDB default charset=utf8;
