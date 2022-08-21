d8f273b2acce2022dfbb18aa6339e305b707f261
crizostomo <diogo_crizostomo@hotmail.com>
2022-08-21 12:21:25 -0300

    7.9 | Creating the migration based on the DDL generated by schema generation - Alter Table in group

--- src/main/resources/db/migration/V004.2__alter_group_table.sql
+++ src/main/resources/db/migration/V004.2__alter_group_table.sql
@@ -2,7 +2,7 @@ alter table group_permission add constraint fk_group_permission_group
 foreign key (group_id) references `group` (id);
 
 alter table user_group add constraint fk_user_group_group
-foreign key (`group`) references `group` (id);
+foreign key (group_id) references `group` (id);
 
 alter table user_group add constraint fk_user_group_user
 foreign key (user_id) references `user` (id);
