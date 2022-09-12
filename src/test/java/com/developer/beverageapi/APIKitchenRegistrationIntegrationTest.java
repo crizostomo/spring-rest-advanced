42470b819cec2a7e78d7bc9baea5ba4548c623c1
crizostomo <diogo_crizostomo@hotmail.com>
2022-09-12 08:57:23 -0300

    10.8 | Implementing API Tests with REST Assured and validating the status code HTTP

---
+++ src/test/java/com/developer/beverageapi/APIKitchenRegistrationIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.developer.beverageapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class APIKitchenRegistrationIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void mustReturnStatus200_WhenConsultingKitchens() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                .basePath("/kitchens")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(200);
+    }
+    /**
+     * To Solve the problem: > No tests found for given includes: [com.developer.beverageapi.KitchenRegistrationIntegrationTest.mustRegisterKitchenSuccessfully](filter.includeTestsMatching)
+     *
+     * I had to use the: testImplementation group: 'io.rest-assured', name: 'rest-assured', version: '4.0.0'
+     *
+     * Instead of: testImplementation group: 'io.rest-assured', name: 'rest-assured', version: '5.2.0'
+     */
+
+}
