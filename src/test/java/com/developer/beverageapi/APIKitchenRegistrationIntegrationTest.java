3b07ccacfe6c49b3ae04460689e2d727e34f2036
crizostomo <diogo_crizostomo@hotmail.com>
2022-09-13 07:44:43 -0300

    10.13 | Setting a DB test and using the Annotation TestPropertySource

--- src/test/java/com/developer/beverageapi/APIKitchenRegistrationIntegrationTest.java
+++ src/test/java/com/developer/beverageapi/APIKitchenRegistrationIntegrationTest.java
@@ -10,10 +10,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.yml")
 public class APIKitchenRegistrationIntegrationTest {
 
     @LocalServerPort
