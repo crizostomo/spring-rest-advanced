b0ec3375f37050e67e216e7253289a5248e50c5e
crizostomo <diogo_crizostomo@hotmail.com>
2022-09-12 21:04:43 -0300

    10.12 | Returning to the initial state in DB for each test execution with Flyway callback

--- src/test/java/com/developer/beverageapi/APIKitchenRegistrationIntegrationTest.java
+++ src/test/java/com/developer/beverageapi/APIKitchenRegistrationIntegrationTest.java
@@ -2,10 +2,12 @@ package com.developer.beverageapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,11 +19,23 @@ public class APIKitchenRegistrationIntegrationTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
+    /**
+     * It was used the Flyway because if the test 'mustReturnStatus201_WhenRecordingKitchen', is executed first
+     * the test 'mustContain3Kitchens_WhenConsultingKitchens' will fail because it will be expecting 3 kitchens
+     * instead of 4 (because one will be persisted after the test 'mustReturnStatus201_WhenRecordingKitchen')
+     * Then, the flyway instance was used in the 'setUp' class since this class is started always before each class
+     */
+
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/kitchens";
+
+        flyway.migrate();
     }
 
     @Test
