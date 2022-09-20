af3560b91fdb608bfd0fb527cf3ee3a5428849cd
crizostomo <diogo_crizostomo@hotmail.com>
2022-09-20 09:41:15 -0300

    11.14 | Adding and using the ModelMapper

--- src/main/java/com/developer/beverageapi/assembler/RestaurantInputDismantle.java
+++ src/main/java/com/developer/beverageapi/assembler/RestaurantInputDismantle.java
@@ -2,22 +2,30 @@ package com.developer.beverageapi.assembler;
 
 import com.developer.beverageapi.domain.model.Kitchen;
 import com.developer.beverageapi.domain.model.Restaurant;
+import com.developer.beverageapi.domain.model.RestaurantModel;
 import com.developer.beverageapi.domain.model.input.RestaurantInput;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestaurantInputDismantle {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurant toDomainObject(RestaurantInput restaurantInput) {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName(restaurantInput.getName());
-        restaurant.setDelivery(restaurantInput.getDelivery());
+//        Restaurant restaurant = new Restaurant();
+//        restaurant.setName(restaurantInput.getName());
+//        restaurant.setDelivery(restaurantInput.getDelivery());
+//
+//        Kitchen kitchen = new Kitchen();
+//        kitchen.setId(restaurantInput.getKitchen().getId());
+//
+//        restaurant.setKitchen(kitchen);
+//
+//        return restaurant;
 
-        Kitchen kitchen = new Kitchen();
-        kitchen.setId(restaurantInput.getKitchen().getId());
-
-        restaurant.setKitchen(kitchen);
-
-        return restaurant;
+        return modelMapper.map(restaurantInput, Restaurant.class);
     }
 }
--- src/main/java/com/developer/beverageapi/assembler/RestaurantModelAssembler.java
+++ src/main/java/com/developer/beverageapi/assembler/RestaurantModelAssembler.java
@@ -3,6 +3,8 @@ package com.developer.beverageapi.assembler;
 import com.developer.beverageapi.domain.model.KitchenModel;
 import com.developer.beverageapi.domain.model.Restaurant;
 import com.developer.beverageapi.domain.model.RestaurantModel;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,17 +13,23 @@ import java.util.stream.Collectors;
 @Component
 public class RestaurantModelAssembler {
 
-    public RestaurantModel toModel(Restaurant restaurant) {
-        KitchenModel kitchenModel = new KitchenModel();
-        kitchenModel.setId(restaurant.getKitchen().getId());
-        kitchenModel.setName(restaurant.getKitchen().getName());
+    @Autowired
+    private ModelMapper modelMapper;
 
-        RestaurantModel restaurantModel = new RestaurantModel();
-        restaurantModel.setId(restaurant.getId());
-        restaurantModel.setName(restaurant.getName());
-        restaurantModel.setDelivery(restaurant.getDelivery());
-        restaurantModel.setKitchen(kitchenModel);
-        return restaurantModel;
+    public RestaurantModel toModel(Restaurant restaurant) {
+//        KitchenModel kitchenModel = new KitchenModel();
+//        kitchenModel.setId(restaurant.getKitchen().getId());
+//        kitchenModel.setName(restaurant.getKitchen().getName());
+//
+//        RestaurantModel restaurantModel = new RestaurantModel();
+//        restaurantModel.setId(restaurant.getId());
+//        restaurantModel.setName(restaurant.getName());
+//        restaurantModel.setDelivery(restaurant.getDelivery());
+//        restaurantModel.setKitchen(kitchenModel);
+//        return restaurantModel;
+
+        return modelMapper.map(restaurant, RestaurantModel.class);
+
     }
 
     public List<RestaurantModel> toCollectionModel(List<Restaurant> restaurants) {
