3e921562f639ca65e4289ce34739d762825c00fd
crizostomo <diogo_crizostomo@hotmail.com>
2022-10-22 20:04:58 -0300

    13.3 | Limiting the fields returned by the API with Squiggly

---
+++ src/main/java/com/developer/beverageapi/core/squiggly/SquigglyConfig.java
@@ -0,0 +1,30 @@
+package com.developer.beverageapi.core.squiggly;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.web.RequestSquigglyContextProvider;
+import com.github.bohnman.squiggly.web.SquigglyRequestFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SquigglyConfig {
+
+    @Bean
+    public FilterRegistrationBean<SquigglyRequestFilter> squigglyRequestFilter(ObjectMapper objectMapper) {
+        Squiggly.init(objectMapper, new RequestSquigglyContextProvider());
+
+//        var urlPatterns = Arrays.asList("/orders/*", "/restaurants/*"); // If we want to allow this config only for these URLs
+
+        var filterRegistration = new FilterRegistrationBean<SquigglyRequestFilter>();
+        filterRegistration.setFilter(new SquigglyRequestFilter());
+        filterRegistration.setOrder(1);
+
+//        filterRegistration.setUrlPatterns(urlPatterns);
+
+        return filterRegistration;
+    }
+}
