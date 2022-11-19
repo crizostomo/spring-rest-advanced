13026b2b5cb70c5fa914b00254654ca052192087
crizostomo <diogo_crizostomo@hotmail.com>
2022-11-18 21:39:21 -0300

    17.5 | Implementing conditionals requirements with Shallow Etags

--- src/main/java/com/developer/beverageapi/core/web/WebConfig.java
+++ src/main/java/com/developer/beverageapi/core/web/WebConfig.java
@@ -1,8 +1,12 @@
 package com.developer.beverageapi.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
 //                .allowedOrigins("http://localhost:8080/restaurants")
 //                .maxAge(30)
                 .allowedMethods("*");
+    }
+
+    /**
+     * The method below generates a hash in the response and it checks if it matches the 'Etag' that is in the 'if-none-match'
+     * and if it is equal, it returns a 304 status
+     */
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
