f0d5b79099a3486421ddd0fab24a4b5b0276185e
crizostomo <diogo_crizostomo@hotmail.com>
2022-11-20 08:32:42 -0300

    18.5 | Selecting ONLY the API endpoints

--- src/main/java/com/developer/beverageapi/core/openapi/SwaggerConfig.java
+++ src/main/java/com/developer/beverageapi/core/openapi/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.developer.beverageapi.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,14 +12,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.developer.beverageapi.api")) // It can be used Predicates.and
                 .paths(PathSelectors.any())
                 .build();
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath/META-INF/resources/webjars/");
+//    }
 }
