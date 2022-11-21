003c42a8c6c112eb544f815346df0e6499e39e9f
crizostomo <diogo_crizostomo@hotmail.com>
2022-11-20 23:50:37 -0300

    18.12 | Describing status code globally

--- src/main/java/com/developer/beverageapi/core/openapi/SwaggerConfig.java
+++ src/main/java/com/developer/beverageapi/core/openapi/SwaggerConfig.java
@@ -3,18 +3,25 @@ package com.developer.beverageapi.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -28,11 +35,26 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage("com.developer.beverageapi.api")) // It can be used Predicates.and
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cities", "It runs cities"));
     }
 
-    public ApiInfo apiInfo() {
+    private List<ResponseMessage> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message("Internal Error")
+                        .build(),
+                new ResponseMessageBuilder()
+                        .code(HttpStatus.NOT_ACCEPTABLE.value())
+                        .message("Resource does not has representation acceptable by the client")
+                        .build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Beverage Food API")
                 .description("Open API for clients")
