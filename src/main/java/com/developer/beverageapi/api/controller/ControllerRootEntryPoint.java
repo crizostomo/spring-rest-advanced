5890101f66d7c336a95586461a78fb440e1bfd95
crizostomo <diogo_crizostomo@hotmail.com>
2023-01-13 14:33:48 -0300

    19.36 | Implementing the API Root Entry Point

---
+++ src/main/java/com/developer/beverageapi/api/controller/ControllerRootEntryPoint.java
@@ -0,0 +1,37 @@
+package com.developer.beverageapi.api.controller;
+
+import com.developer.beverageapi.api.InstantiateLinks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class ControllerRootEntryPoint {
+
+    @Autowired
+    private InstantiateLinks instantiateLinks;
+
+    @GetMapping
+    public RootEntryPointModel root() {
+        var rootEntryPointModel = new RootEntryPointModel();
+
+        rootEntryPointModel.add(instantiateLinks.linkToKitchens("kitchens"));
+        rootEntryPointModel.add(instantiateLinks.linkToOrders("orders"));
+        rootEntryPointModel.add(instantiateLinks.linkToRestaurants("restaurants"));
+        rootEntryPointModel.add(instantiateLinks.linkToGroups("groups"));
+        rootEntryPointModel.add(instantiateLinks.linkToUsers("users"));
+        rootEntryPointModel.add(instantiateLinks.linkToPermissions("permissions"));
+        rootEntryPointModel.add(instantiateLinks.linkToPayment("payments"));
+        rootEntryPointModel.add(instantiateLinks.linkToStates("states"));
+        rootEntryPointModel.add(instantiateLinks.linkToCities("cities"));
+
+        return rootEntryPointModel;
+    }
+
+    private static class RootEntryPointModel extends RepresentationModel<RootEntryPointModel> {
+    }
+}
