4d7c75c995d133427d6a24a963e37ac0cd818203
crizostomo <diogo_crizostomo@hotmail.com>
2023-01-31 11:37:01 -0300

    23.6 | Configuring the Resource Server for JWT signed with a symmetric key

--- src/main/java/com/developer/beverageapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/developer/beverageapi/core/security/ResourceServerConfig.java
@@ -1,10 +1,15 @@
 package com.developer.beverageapi.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +23,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+//                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("beveragea45sd4s56f4sd5f4d5g5dsf41d5fg4d5f".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
