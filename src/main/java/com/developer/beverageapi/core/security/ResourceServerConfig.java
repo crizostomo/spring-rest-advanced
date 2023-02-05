419baba20b3294681387db05a1a31db5f052edcf
crizostomo <diogo_crizostomo@hotmail.com>
2023-02-04 21:12:39 -0300

    23.20 | Loading the Granted Authorities and restricting access to the endpoints in the API

--- src/main/java/com/developer/beverageapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/developer/beverageapi/core/security/ResourceServerConfig.java
@@ -2,14 +2,19 @@ package com.developer.beverageapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -19,12 +24,34 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+                .antMatchers(HttpMethod.POST,"/v1/kitchens/**").hasAuthority("EDIT_KITCHENS")
+                .antMatchers(HttpMethod.PUT,"/v1/kitchens/**").hasAuthority("EDIT_KITCHENS")
+                .antMatchers(HttpMethod.GET,"/v1/kitchens/**").authenticated()
+                .anyRequest().denyAll()
                 .and()
                 .cors().and()
 //                .oauth2ResourceServer().opaqueToken();
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+    }
+
+    // Method used to extract the authorities in the resource server
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
     }
 
 //    @Bean
