6dd0cf1c92d41e50617f9080085580fe38cc5bc3
crizostomo <diogo_crizostomo@hotmail.com>
2023-03-14 13:19:32 -0300

    27.3 | Initial configuration of the Authorization Server with Opaque Token

---
+++ src/main/java/com/developer/beverageapi/core/security/authorizationserver/BeverageSecurityProperties.java
@@ -0,0 +1,20 @@
+package com.developer.beverageapi.core.security.authorizationserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Component
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("beverage.auth")
+public class BeverageSecurityProperties {
+
+    @NotBlank
+    private String provideUrl;
+}
