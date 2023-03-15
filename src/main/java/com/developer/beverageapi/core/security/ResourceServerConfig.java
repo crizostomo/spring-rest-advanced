61498c85a717f0b55fa649a568420b65c5c2f519
crizostomo <diogo_crizostomo@hotmail.com>
2023-03-15 17:45:22 -0300

    27.6 | Configuring the Resource Server with Opaque Token

---
+++ src/main/java/com/developer/beverageapi/core/security/ResourceServerConfig.java
@@ -0,0 +1,30 @@
+package com.developer.beverageapi.core.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/oauth2/**").authenticated()
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                .oauth2ResourceServer().opaqueToken();
+
+        return http.build();
+    }
+
+}
